2b8691f0db3c3d859266ecb0f7a2eb0468cb89d4
Peppi <63722327+miapeppi@users.noreply.github.com>
2021-09-04 21:09:40 +0300

    FIX: Correct typos

--- RPG-Characters/Attributes/SecondaryAttribute.cs
+++ RPG-Characters/Attributes/SecondaryAttribute.cs
@@ -14,7 +14,7 @@ namespace RPG_Characters.Charachters
         public int ElementalResistance { get; set; }
         public double DPS { get; set; }
 
-        // Overloaded constructor calculating DPS with no weapon equipped
+        // Overloaded constructor calculating DPS with no weapon equipped.
         public SecondaryAttribute(PrimaryAttribute totalAtribute, double damageAttribute)
         {
             Health = totalAtribute.Vitality * 10;
@@ -24,7 +24,7 @@ namespace RPG_Characters.Charachters
         }
 
         /// <summary>
-        /// Method to update secondary attributes when there is changes in the primary attributes
+        /// Method to update secondary attributes when there is changes in the primary attributes.
         /// </summary>
         /// <param name="totalAtribute">Updated total attributes with e.g. new armor added</param>
         public void Update(PrimaryAttribute totalAtribute)
@@ -35,7 +35,7 @@ namespace RPG_Characters.Charachters
         }
 
         /// <summary>
-        /// Method to update secondary attributes when there is new weapon equipped
+        /// Method to update secondary attributes when there is new weapon equipped.
         /// </summary>
         /// <param name="totalAtribute">Updated total attributes</param>
         /// <param name="damageAttribute">Hero's damage attribute</param>
